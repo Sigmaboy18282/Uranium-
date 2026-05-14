867d6b9d2e409e6b1fda7c4fb14c22288112365c
Sigmaboy18282 <n42cgvw5jq@privaterelay.appleid.com>
2026-05-13 21:30:06 -0400

    Add service worker for UV integration

---
+++ sw.js
@@ -0,0 +1,10 @@
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.sw.js');
+
+const uv = new UVServiceWorker();
+self.addEventListener('fetch', event => {
+    if (event.request.url.startsWith(location.origin + '/service/')) {
+        event.respondWith(uv.fetch(event));
+    }
+});
